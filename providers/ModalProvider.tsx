7f86849b2006bb2a9f6f0561a8137b3220b2aac7
summer030 <125527872+summer030@users.noreply.github.com>
2023-07-25 20:07:54 +0800

    feat: songs fetching and list display

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import AuthModal from "@/app/components/AuthModal";
-import UploadModal from "@/app/components/UploadModal";
+import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
