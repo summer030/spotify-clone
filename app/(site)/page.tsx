6e0cb10bbe75de58f9b84b9aac36ec6a4c9ff5db
summer030 <125527872+summer030@users.noreply.github.com>
2023-07-26 22:22:58 +0800

    fix: router error

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
             <ListItem
               image="/images/liked.png"
               name="Liked Songs"
-              href="iked"
+              href="liked"
             />
           </div>
         </div>
